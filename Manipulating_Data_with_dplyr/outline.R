7263151c8941f233f802c3d29a4db4203da293c1
Eric Koncina <mail@koncina.eu>
2018-01-05 22:47:06 +0100

    add adapted dplyr lesson

---
+++ Manipulating_Data_with_dplyr/outline.R
@@ -0,0 +1,49 @@
+### Manipulating data with dplyr - Getting and Cleaning Data ###
+
+# Code to download data
+# logurl <- 'http://cran-logs.rstudio.com/2014/2014-07-08.csv.gz'
+# filepath <- paste0('~/', basename(logurl))
+# download.file(logurl, filepath)
+# cranlog <- read.csv(filepath)
+# write.csv(cranlog, '~/Desktop/2014-07-08.csv')
+# unlink(filepath)
+
+path2csv <- file.path('~/Desktop/2014-07-08.csv')
+
+
+### Intro
+cran <- read_csv(path2csv)
+library(tidyverse)
+cran
+?manip
+
+# "The dplyr philosophy is to have small functions that each do
+# one thing well."
+
+
+### select()
+select(cran, ip_id, package, country) # reorders
+select(cran, r_arch:country)
+select(cran, -(X:time))
+select(cran, ip_id:size)
+
+### filter()
+filter(cran, package == "swirl")
+filter(cran, r_version == "3.1.1", country == "US")
+filter(cran, country == "US" | country == "CA")
+filter(cran, r_version >= "3.0.0", r_os == "linux-gnu")
+filter(cran, !is.na(package))
+
+### arrange()
+arrange(cran, ip_id)
+arrange(cran, package, ip_id)
+arrange(cran, country, desc(r_version), ip_id)
+arrange(cran, package, desc(version), ip_id)
+
+### mutate()
+cran1 <- select(cran, ip_id, package, size)
+mutate(cran1, size_mb = size / 2^20)
+mutate(cran1, size_mb = size / 2^20, size_gb = size_mb / 2^10)
+
+### summarize()
+summarise(cran, avg_bytes = mean(size))
